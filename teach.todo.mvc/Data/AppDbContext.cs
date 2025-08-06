ae52255dd229dde181f98532d9b563db76c2054a
Brad Norris <brad.norris@live.com>
2025-08-05 20:40:24 -0500

    Setup entity framework

---
+++ teach.todo.mvc/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using teach.todo.mvc.Models;
+
+namespace teach.todo.mvc.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    }
+}
