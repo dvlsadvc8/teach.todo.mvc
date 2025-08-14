e2fdc15d60af5c1daf6d399d474d770c9062e0e5
Brad Norris <brad.norris@live.com>
2025-08-14 17:43:26 -0500

    Added Todo validation and some styling.

--- teach.todo.mvc/Models/TodoItem.cs
+++ teach.todo.mvc/Models/TodoItem.cs
@@ -1,8 +1,14 @@
-﻿namespace teach.todo.mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace teach.todo.mvc.Models
 {
     public class TodoItem
     {
         public int Id { get; set; }
+
+        // These [] elements are called Validation Attributes
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters")]
         public string Title { get; set; } = string.Empty;
         public bool IsComplete { get; set; }
     }
